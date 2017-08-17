5ddc7ac1f56db1941cafbff4fcae82e75e5a80c3
shindedigambar <30692635+shindedigambar@users.noreply.github.com>
2017-08-17 20:53:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,54 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne = {
+    title: 'Article one! Digambar Shinde'
+    heading: 'Article one'
+    date: 'August 17, 2017'
+    content: '                                                                                                              <p>                                                                                                                     This is a practicle session of Introduction to modern applicatio development. This is a practicle session of         Introduction to modern applicatio development. This is a practicle session of Introduction to modern               applicatio development. This is a practicle session of Introduction to modern applicatio development. This         is a practicle session of Introduction to modern applicatio development. This is a practicle session of            Introduction to modern applicatio development. This is a practicle session of Introduction to modern               applicatio development.                                                                                     </p>                                                                                                               <p>                                                                                                                     This is a practicle session of Introduction to modern applicatio development. This is a practicle session of         Introduction to modern applicatio development. This is a practicle session of Introduction to modern               applicatio development. This is a practicle session of Introduction to modern applicatio development. This         is a practicle session of Introduction to modern applicatio development. This is a practicle session of            Introduction to modern applicatio development. This is a practicle session of Introduction to modern               applicatio development.                                                                                     </p>                                                                                                               <p>                                                                                                                     This is a practicle session of Introduction to modern applicatio development. This is a practicle session of         Introduction to modern applicatio development. This is a practicle session of Introduction to modern               applicatio development. This is a practicle session of Introduction to modern applicatio development. This         is a practicle session of Introduction to modern applicatio development. This is a practicle session of            Introduction to modern applicatio development. This is a practicle session of Introduction to modern               applicatio development.                                                                                     </p>'
+};
+function creatTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = '                                                                                                <html>
+            <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1 ">
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+                <div>
+                    ${content}
+                </div>
+            </body>
+            </div>
+    </html>
+    ';
+    return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
